ff99622df58338b4e30affc60cf99400d4e0330c
Nestor1984 <144503353+Nestor1984@users.noreply.github.com>
2023-12-17 18:11:32 -0800

    Add files via upload

---
+++ 2.-Variables.py
@@ -0,0 +1,12 @@
+nombre = "Nestor Jhoel"
+print(nombre)
+
+edad = 20
+print(edad)
+
+edad = True
+print(edad)
+
+sueldo = 205.10
+print(sueldo)
+
---
+++ 4.-Operaciones.py
@@ -0,0 +1,32 @@
+
+entero = 23
+decimal = 31.78
+complejo = 12 + 5j # Python soporta numero complejos
+booleano = True
+
+"""
+Imprimimos los valores
+"""
+
+print(entero)
+print(decimal)
+print(complejo)
+print(booleano)
+
+#Suma, resta, multiplicacion, division, division exacta y potencia
+num1 = 20
+num2 = 4
+print("Suma:", (num1 + num2)) # En python se concatena con coma, la coma crea un espacio entre el texto y la suma entre parentesis
+print("Resta:", (num1 - num2))
+print("Multiplicacion:", (num1 * num2))
+print("Division:", (num1 / num2))
+print("Division:", (num1 // num2))
+print("Potencia:", (num1 ** num2))
+"""
+Salida:
+Suma: 24
+Resta: 16
+Multiplicacion: 80
+Division: 5.0
+Potencia: 160000
+"""
